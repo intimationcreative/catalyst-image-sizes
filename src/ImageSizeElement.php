a7187cc1ab31b1d51892f4ed903a5acf42c3001c
Craig Simpson <craig.simpson@intimation.co.uk>
2023-06-19 15:31:06 +0100

    Initial commit

---
+++ src/ImageSizeElement.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Intimation\Catalyst;
+
+class ImageSizeElement extends Component {
+
+    const ADD = 'add';
+    const REMOVE = 'remove';
+
+    public function init() {
+        if ( array_key_exists( self::REMOVE, $this->config)) {
+            array_map( 'remove_image_size', $this->config[ self::REMOVE ] );
+        }
+
+        if( array_key_exists(self::ADD, $this->config)) {
+            array_walk( $this->config[ self::ADD ], function( $args, $name ) {
+                add_image_size( $name, $args[0], $args[1], isset( $args[2] ) ? $args[2] : false );
+            } );
+        }
+    }
+}
